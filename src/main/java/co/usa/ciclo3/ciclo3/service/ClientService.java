7c25ee04e1c6455527892427708aa74415b28a22
CarlosBernalMT <carlos.bernal.mt@correo.usa.edu.co>
2021-11-05 12:42:53 -0500

    Agregado Client CRUD

---
+++ src/main/java/co/usa/ciclo3/ciclo3/service/ClientService.java
@@ -0,0 +1,38 @@
+package co.usa.ciclo3.ciclo3.service;
+
+import co.usa.ciclo3.ciclo3.model.Car;
+import co.usa.ciclo3.ciclo3.model.Client;
+import co.usa.ciclo3.ciclo3.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public List<Client> getALL(){
+        return  clientRepository.getALL();
+    }
+
+    public Optional<Client> getClient(int id){
+        return clientRepository.getClient(id);
+    }
+
+    public Client save(Client c){
+        if (c.getId()==null){
+            return clientRepository.save(c);
+        }else{
+            Optional<Client> caux = clientRepository.getClient(c.getId());
+            if (caux.isEmpty()){
+                return clientRepository.save(c);
+            }else{
+                return c;
+            }
+        }
+    }
+}
